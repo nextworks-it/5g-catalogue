e13b6b6e446235b7cf26402eeccf857911c83345
Francesca Moscatelli <f.moscatelli@nextworks.it>
2018-09-12 10:24:49 +0200

    TOSCA UHD vCDN NSD added

--- Catalogue/test/sample_loadNsdContent.sh
+++ Catalogue/test/sample_loadNsdContent.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -v -X PUT --data-binary @vCDN_tosca_v02.yaml http://localhost:8083/nsd/v1/ns_descriptors/$1/nsd_content --header "Content-Type:application/x-yaml" --header "Accept:application/json"
+curl -v -X PUT --data-binary @$2 http://localhost:8083/nsd/v1/ns_descriptors/$1/nsd_content --header "Content-Type:application/x-yaml" --header "Accept:application/json"
 
 curl -v -X GET http://localhost:8083/nsd/v1/ns_descriptors/$1/nsd_content --header "Content-Type:application/x-yaml" --header "Accept:application/x-yaml"
