45e077cc1730b50d4be1dd29d32b62fca2239e79
Leonardo Agueci <l.agueci@nextworks.it>
2019-03-18 12:18:35 +0100

    Fix on Scaling group translation

--- 5gcatalogue-app/src/main/java/it/nextworks/nfvmano/catalogue/plugins/mano/osm/common/VnfdBuilder.java
+++ 5gcatalogue-app/src/main/java/it/nextworks/nfvmano/catalogue/plugins/mano/osm/common/VnfdBuilder.java
@@ -97,8 +97,8 @@ public class VnfdBuilder {
 
         List<ScalingGroupDescriptor> descriptors = new ArrayList<>();
         ScalingGroupDescriptor scalingByOne = new ScalingGroupDescriptor();
-        scalingByOne.setMinInstanceCount(vdu.getProperties().getVduProfile().getMinNumberOfInstances() - 1);
-        scalingByOne.setMaxInstanceCount(vdu.getProperties().getVduProfile().getMaxNumberOfInstances() - 1);
+        scalingByOne.setMinInstanceCount(0);
+        scalingByOne.setMaxInstanceCount(vdu.getProperties().getVduProfile().getMaxNumberOfInstances() - vdu.getProperties().getVduProfile().getMinNumberOfInstances());
         scalingByOne.setName("scale_by_one");
         List<ScalingPolicy> scalingPolicies = new ArrayList<>();
         ScalingPolicy scalingPolicy = new ScalingPolicy("manual_scale", "manual");
