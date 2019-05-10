aefbe8889bb91cc483599f27228dc7d01ce5ecc4
Francesca Moscatelli <f.moscatelli@nextworks.it>
2019-05-10 15:25:45 +0200

    Fix in MANO plugins deserialization

--- 5gcatalogue-app/src/main/java/it/nextworks/nfvmano/catalogue/plugins/mano/MANO.java
+++ 5gcatalogue-app/src/main/java/it/nextworks/nfvmano/catalogue/plugins/mano/MANO.java
@@ -28,6 +28,7 @@ import javax.persistence.Id;
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "manoType", visible = true)
 @JsonSubTypes({@JsonSubTypes.Type(value = OSMMano.class, name = "OSMR3"),
         @JsonSubTypes.Type(value = OSMMano.class, name = "OSMR4"),
+        @JsonSubTypes.Type(value = OSMMano.class, name = "OSMR5"),
         @JsonSubTypes.Type(value = DummyMano.class, name = "DUMMY")})
 public abstract class MANO {
 
