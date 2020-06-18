a0884635b6b291f0e2bf79c1ebc9ca01685bf1ed
Leonardo Agueci <l.agueci@nextworks.it>
2020-06-18 16:09:41 +0200

    Fixes on proxy configuration

--- deployments/docker/scripts/deploy-with-proxy.sh
+++ deployments/docker/scripts/deploy-with-proxy.sh
@@ -2,6 +2,7 @@
 
 mkdir -p ~/.docker/
 touch ~/.docker/config.json
-echo "{\"proxies\":{\"default\":{\"httpProxy\":\"http://163.162.8.137:9777\",\"httpsProxy\":\"http://163.162.8.137:9777\"}}}" > ~/.docker/config.json
+echo "{\"proxies\":{\"default\":{\"httpProxy\":\"http://163.162.8.137:9777\",\"httpsProxy\":\"http://163.162.8.137:9777\",\"noProxy\":\"172.17.254.0/24,172.17.105.0/24,10.50.7.0/24\"}}}" > ~/.docker/config.json
 
 docker-compose -f "docker-compose.yml" up -d --build
+172.17.254.0/24
